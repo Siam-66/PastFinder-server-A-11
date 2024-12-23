7f73e6bf990bbd70de67033de8992e9221e1d0ef
Siam-66 <siammahmud66@gmail.com>
2024-12-24 02:43:49 +0600

    app.get

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require ('cors');
 const app = express();
 require ('dotenv').config()
 const port = process.env.PORT || 4000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -14,24 +14,42 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
+serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+}
 });
 
 async function run() {
-  try {
+try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
+
+    const celestoraCollection = client.db('pastFinderDB').collection('celestora')
+
+    // data fetch
+    app.get('/celestora', async(req,res)=>{
+        const cursor = celestoraCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    // data details
+    app.get('/celestora/:id', async(req, res)=> {
+        const id = req.params.id;
+        const query={_id: new ObjectId(id)}
+        const result =await celestoraCollection.findOne(query);
+        res.send(result);
+    })
+
+} finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    // await client.close();
+}
 }
 run().catch(console.dir);
 
